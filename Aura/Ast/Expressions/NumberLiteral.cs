0af05748bda67ceaf79140d3cba8a852ce6e596c
Rain336 <rain336@live.de>
2017-09-15 13:30:53 +0200

    Re-added NumberLiterals

---
+++ Aura/Ast/Expressions/NumberLiteral.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Aura.Ast.Expressions
+{
+    public sealed class NumberLiteral : IExpression
+    {
+        public string Text { get; }
+        public IType ResultType { get; }
+
+        public NumberLiteral(string data, bool hexa)
+        {
+            Text = data;
+            ResultType = GetType(data, hexa);
+        }
+
+        public static IType GetType(string data, bool hexa)
+        {
+            if (hexa)
+            {
+                switch (Convert.ToInt64(data.Substring(2), 16))
+                {
+                    case long i when i < byte.MaxValue:
+                        return SimpleType.Byte;
+
+                    case long i when i < short.MaxValue:
+                        return SimpleType.Short;
+
+                    case long i when i < int.MaxValue:
+                        return SimpleType.Int;
+
+                    default:
+                        return SimpleType.Long;
+                }
+            }
+
+            if (data.Contains("."))
+            {
+                var f = Convert.ToDouble(data);
+                if (f < float.MaxValue)
+                    return SimpleType.Float;
+                else
+                    return SimpleType.Double;
+            }
+
+            switch (Convert.ToInt64(data))
+            {
+                case long i when i < byte.MaxValue:
+                    return SimpleType.Byte;
+
+                case long i when i < short.MaxValue:
+                    return SimpleType.Short;
+
+                case long i when i < int.MaxValue:
+                    return SimpleType.Int;
+
+                default:
+                    return SimpleType.Long;
+            }
+        }
+    }
+}
--- Aura/Ast/Type.cs
+++ Aura/Ast/Type.cs
@@ -15,6 +15,20 @@ namespace Aura.Ast
         public static readonly SimpleType Any = new SimpleType { Text = "Aura.Any" };
         public static readonly SimpleType Void = new SimpleType { Text = "Aura.Void" };
         public static readonly SimpleType String = new SimpleType { Text = "Aura.String" };
+
+        public static readonly SimpleType Float = new SimpleType { Text = "Aura.Float" };
+        public static readonly SimpleType Double = new SimpleType { Text = "Aura.Double" };
+
+        public static readonly SimpleType Byte = new SimpleType { Text = "Aura.Byte" };
+        public static readonly SimpleType SByte = new SimpleType { Text = "Aura.SByte" };
+        public static readonly SimpleType Short = new SimpleType { Text = "Aura.Short" };
+        public static readonly SimpleType UShort = new SimpleType { Text = "Aura.UShort" };
+        public static readonly SimpleType Int = new SimpleType { Text = "Aura.Int" };
+        public static readonly SimpleType UInt = new SimpleType { Text = "Aura.UInt" };
+        public static readonly SimpleType Long = new SimpleType { Text = "Aura.Long" };
+        public static readonly SimpleType ULong = new SimpleType { Text = "Aura.ULong" };
+        public static readonly SimpleType Size = new SimpleType { Text = "Aura.Size" };
+        public static readonly SimpleType USize = new SimpleType { Text = "Aura.USize" };
     }
 
     public sealed class GenericType : IType
